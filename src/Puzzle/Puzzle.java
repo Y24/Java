5e63949674c559ae8bdfb17434f39cc6fbd6b3bf
Y24 <Y24@users.noreply.github.com>
2018-04-24 17:10:34 +0800

    puzzle

--- src/Puzzle/Puzzle.java
+++ src/Puzzle/Puzzle.java
@@ -1,14 +1,35 @@
 package Puzzle;
 
 import java.awt.*;
+import java.awt.event.*;
 
 public class Puzzle extends Frame {
     public static void main(String[] argc){
         Puzzle MainWindow=new Puzzle();
+        MainWindow.setSize(new Dimension(600,600));
+        MainWindow.setTitle("15-square Puzzle");
+        MainWindow.setVisible(true);
     }
     public Puzzle(){
-        addKeyListener();
-        addMouseListener();
-        addWindowListener();
+        addKeyListener(new KeyAdapter() {
+        });
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+        });
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        setLayout(new GridLayout(4,4));
+        setFont(new Font("SansSerif",Font.BOLD,24));
+        for(int i=1;i<16;i++)
+                add(new Button("" + i));
+        add(new Button());
+            }
     }
-}
+
